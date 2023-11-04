32f67a8861da147625fb6065b657e68b0a6206e4
Kettengeschwindigkeit <666damned999@mail.ru>
2023-11-04 20:34:43 +0300

    Animated Sidebar (Hamburger Menu Animation)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
+import { motion } from "framer-motion";
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
+      <Sidebar />
       <div className="wrapper">
-        <span>Kettengeschwindigkeit</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Kettengeschwindigkeit
+          </motion.span>
         <div className="social">
           <a href="#"><img src="/facebook.png" alt="" /></a>
           <a href="#"><img src="/instagram.png" alt="" /></a>
