a61b4437cfb5cf0ea666e0ac9abcb0e1e10bd589
Kettengeschwindigkeit <666damned999@mail.ru>
2023-11-05 17:33:31 +0300

    Animate In View (useInView hook)

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,31 +1,60 @@
-import { motion } from "framer-motion";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import "./services.scss";
 
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1
+    }
+  }
+};
+
 const Services = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: "-100px" });
+
   return (
-    <motion.div className="services">
-      <motion.div className="text-container">
+    <motion.div
+      ref={ref}
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      animate={isInView && "animate"}
+      >
+      <motion.div className="text-container" variants={variants}>
         <p>
           I focus on helping your brand grow
           <br /> and move forward
         </p>
         <hr />
       </motion.div>
-      <motion.div className="title-container">
+      <motion.div className="title-container" variants={variants}>
         <div className="title">
           <img src="/people.webp" alt="" />
           <h1>
-            <b>Unique</b> Ideas
+            <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <b>For Your</b> Business.
+            <motion.b whileHover={{ color: "orange" }}>For Your</motion.b> Business.
           </h1>
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
-      <motion.div className="list-container">
+      <motion.div className="list-container" variants={variants}>
         <motion.div className="box" whileHover={{ color: "black", backgroundColor: "lightgray" }}>
           <h2>Branding</h2>
           <p>
