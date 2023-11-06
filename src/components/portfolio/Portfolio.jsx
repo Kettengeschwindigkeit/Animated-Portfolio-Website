9889950745aa2b88fd5365c346c52021c7a1484f
Kettengeschwindigkeit <666damned999@mail.ru>
2023-11-06 17:47:37 +0300

    Animated Progress Bar

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+import { motion, useScroll, useSpring } from "framer-motion";
 import "./portfolio.scss";
 
 const items = [
@@ -32,11 +34,22 @@ const Single = ({ item }) => {
 };
 
 const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref, offset: ["end end", "start start"]
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30
+  });
+
   return (
-    <div className="portfolio">
+    <div className="portfolio" ref={ref}>
       <div className="progress">
         <h1>Featured Works</h1>
-        <div className="progress-bar"></div>
+        <motion.div className="progress-bar" style={{ scaleX }}></motion.div>
       </div>
       {items.map(item => (
         <Single item={item} key={item.id}  />
